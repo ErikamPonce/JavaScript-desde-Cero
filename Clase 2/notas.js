62044375530bcfab2633af76e4bfb17eb1da4347
Erikam Ponce <americaalarconponce@gmail.com>
2026-04-30 12:50:51 -0600

    ejemplos en clase

--- Clase 2/notas.js
+++ Clase 2/notas.js
@@ -18,3 +18,54 @@ if (nota >= 0 && nota <= 100) {
 } else {
     console.log("Error: La nota debe estar entre 0 y 100");
 }
+
+
+//apuentes clases
+
+//declarando una variable puntuacion
+let puntuacion = 56
+//reasignando el valor de la variable puntuacion
+puntuacion= 60
+//imprimiendo el valor de la variable puntuacion
+console.log(puntuacion);
+
+
+const pais= "Colombia"
+pais= "Argentina"
+//Concatenacion de texto con la variable
+console.log("El país es: " + pais);
+
+
+//boleanos
+let activo= true
+let inactivo= false
+
+//indefinido
+let x
+console.log(x); // Imprime undefined
+
+//nulo
+let y = null
+console.log(y); 
+
+//objetos
+let persona = {
+    nombre: "Juan",
+    edad: 30,
+    activo: true   
+}
+console.log(persona.nombre); // Solo imprime "Juan", osea el nombre de la persona
+
+//arreglos
+let frutas = ["manzana", "banana", "naranja"]
+console.log(frutas[0]); // Imprime "manzana", osea la primera fruta del arreglo
+
+//matrices
+let frutasMatriz = [["manzana", "banana", "naranja"], ["fresa", "granada", "cereza"]]
+console.log(frutasMatriz[0][0]); // Imprime "fresa", osea la primera fruta de la segunda fila de la matriz  
+
+//funciones
+function saludar() {
+    return "Hola mundo";
+}
+saludar(); 
