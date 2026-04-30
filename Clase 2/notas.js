dc6e76d0b2d80be7c45ece19d1aec3eaaa2e2426
Erikam Ponce <americaalarconponce@gmail.com>
2026-04-30 12:05:17 -0600

    practica comparaciones y desiciones js

---
+++ Clase 2/notas.js
@@ -0,0 +1,20 @@
+// variable que almacenará la nota del estudiante
+let nota = 85;
+
+// Validar que la nota sea mayor o igual a 0
+if (nota >= 0 && nota <= 100) {
+    console.log("Su nota es: " + nota);
+
+    if (nota >= 90) {
+        console.log("Excelente");
+    } else if (nota >= 75) {
+        console.log("Buena");
+    } else if (nota >= 60) {
+        console.log(" Suficiente ");
+    } else {
+        console.log(" No apruebado ");
+    }
+
+} else {
+    console.log("Error: La nota debe estar entre 0 y 100");
+}
